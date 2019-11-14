909bd932a987eab9a54efa12fc1512b3c655aa84
yang1127 <1536520643@qq.com>
2019-11-14 23:45:38 +0800

    test06

---
+++ day06/day06/test06.cpp
@@ -0,0 +1,69 @@
+//#include<iostream>
+//#include<vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int w, h;
+//	int count = 0;
+//	cin >> w >> h;
+//
+//	if (w % 4 == 0 || h % 4 == 0) //长或宽除4余数为0时，占有长*宽/2的蛋糕
+//	{
+//		count = w * h / 2;  
+//	}
+//	else if (w % 2 == 0 || h % 2 == 0) //长或宽除2余数为0时，占有（长*宽/4 +1）* 2的蛋糕
+//	{
+//		count = (w * h / 4 + 1) * 2;
+//
+//	}
+//	else
+//	{
+//		count = w * h / 2 + 1;  //长、宽都是奇数的情况
+//	}
+//	cout << count << endl;
+//	return 0;
+//}
+
+
+//class Solution {
+//public:
+//	int StrToInt(string str) {
+//		int result = 0;
+//		int len = str.size();
+//		if (len <= 0)
+//			return 0;
+//		int i = 0;
+//		int flag = 0;
+//		if (str[i] == ' ')
+//		{
+//			i++;
+//		}
+//		if (str[i] == '+')
+//		{
+//			flag = 1;
+//			i++;
+//		}
+//		else if (str[i] == '-')
+//		{
+//			flag = -1;
+//			i++;
+//		}
+//		while (str[i] != '\0')
+//		{
+//			if (str[i] >= '0' && str[i] <= '9')
+//			{
+//				result = result * 10 + (str[i] - '0');
+//				i++;
+//			}
+//			else {
+//				result = 0;
+//				break;
+//			}
+//		}
+//		if (flag == -1)
+//			result = -result;
+//		return result;
+//	}
+//};
