6751e5b94e4a24993dc5b16c271507190090cb0f
yang1127 <1536520643@qq.com>
2019-12-01 23:54:49 +0800

    test

---
+++ day14/day14/test.cpp
@@ -0,0 +1,28 @@
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+string reverseString(string s) 
+{
+	if (s.empty())
+		return s; 
+	size_t start = 0;
+	size_t end = s.size() - 1;
+	while (start < end)
+	{
+		swap(s[start], s[end]);
+		++start; --end;
+	}
+	return s;
+}
+
+int main()
+{
+	string s; 
+	getline(cin,s);
+	cout<<reverseString(s)<<endl;
+
+	system("pause");
+	return 0;
+}
