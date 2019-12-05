d4843a7538d2fc881e8c2fe85f86aa05b738da49
yang1127 <1536520643@qq.com>
2019-12-05 22:34:27 +0800

    test

---
+++ day16/day16/test.cpp
@@ -0,0 +1,62 @@
+//Ê÷¸ù
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int num;
+//	int num1 = 0;
+//	int sum = 0;
+//	cin >> num;
+//
+//	while (num > 1)
+//	{
+//		num1 = num % 10;
+//		num = num / 10;
+//		sum = num + num1;
+//
+//		if (sum < 10)
+//		{
+//			cout << sum << endl;
+//			break;
+//		}
+//		else
+//		{
+//			num = sum;
+//			sum = 0;
+//		}
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
+
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string s;
+//	int i;
+//	int sum;
+//
+//	while (1)
+//	{
+//		cin >> s;
+//		if (s[0] == '0')
+//			break;
+//
+//		for (i = 0, sum = 0; s[i] != '\0'; i++)
+//		{
+//			sum += s[i] - '0';
+//		}
+//
+//		cout << (sum - 1) % 9 + 1 << endl;
+//	}
+//	
+//	system("pause");
+//	return 0;
+//}
+
