84429e47a062fa2d9ea3d23437b33df32ac66985
yang1127 <1536520643@qq.com>
2020-03-16 23:23:40 +0800

    练习

--- day25/day25/test.cpp
+++ day25/day25/test.cpp
@@ -165,3 +165,73 @@
 //	return 0;
 //}
 
+//剪花布条
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string s, t;
+//	while (cin >> s >> t)
+//	{
+//		int res = 0;
+//		size_t pos = 0;
+//		//依次在 s 中查找 t 即可，直到再也找不到 t
+//		while ((pos = s.find(t, pos)) != string::npos)
+//		{
+//			pos += t.size(); //跳过t串整体的长度
+//			++res; //计数
+//		}
+//		cout << res << std::endl;
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+//客似云来
+
+#include <iostream>
+#define MAX 83 
+
+using namespace std;
+
+void solve(long long num[])
+{
+	for (int i = 2; i < MAX; i++)
+	{
+		num[i] = num[i - 1] + num[i - 2];
+	}
+}
+
+long long sum_traversal(long long num[], int from, int to) //解法1：用遍历求和求解
+{
+	long long ans = 0;
+	for (int i = from - 1; i < to; i++) //让数组下标从from - 1遍历到to - 1
+	{
+		ans += num[i];
+	}
+	return ans;
+}
+
+long long sum_formula(long long num[], int from, int to) //解法2：用公式求解
+{
+	return num[to + 1] - num[from]; //第to + 2项的下标是to + 1，第from + 2 - 1项的下标是from
+}
+
+int main()
+{
+	int from, to;
+	long long num[MAX] = { 1, 1 };
+	solve(num);
+	while (cin >> from >> to)
+	{
+		cout << sum_formula(num, from, to) << endl; 
+	}
+
+	system("pause");
+	return 0;
+}
