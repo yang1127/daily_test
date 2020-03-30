dc46d391c94314717376ea4d86d52274dbced910
yang1127 <1536520643@qq.com>
2020-03-30 21:13:41 +0800

    练习

--- day01/day01/test01.cpp
+++ day01/day01/test01.cpp
@@ -209,41 +209,211 @@
 //	return 0;
 //}
 
-//乒乓球赛
-#include<iostream>
-#include<string>
-#include<map>
+////乒乓球赛
+//#include<iostream>
+//#include<string>
+//#include<map>
+//using namespace std;
+//
+//int main()
+//{
+//	string s1, s2;
+//	while (cin >> s1 >> s2)
+//	{
+//		bool status = true;
+//		map<char, int> str1, str2;
+//		for (int i = 0; i < s1.size(); i++)
+//			str1[s1[i]]++;
+//
+//		for (int j = 0; j < s2.size(); j++)
+//			str2[s2[j]]++;
+//
+//		map<char, int>::iterator it = str2.begin();
+//		while (it != str2.end())
+//		{
+//			if (str1[it->first] < it->second) //str1 < str2长度则退出，否则继续往下走
+//			{
+//				status = false;
+//				break;
+//			}
+//			++it;
+//		}
+//
+//		if (status== true )
+//			cout << "Yes" << endl;
+//		else
+//			cout << "No" << endl;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a;
+//	cin >> a;
+//	int a1 = a / 100;
+//	int a2 = a / 10 % 10;
+//	int a3 = a % 10;
+//	int sum = a1 * a1 * a1 + a2 * a2 * a2 + a3 * a3 * a3;
+//	printf("%d", sum);
+//
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int i, j, a = 0;
+//	for (i = 0; i < 2; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			if (j % 2)
+//				break;
+//			a++;
+//		}
+//		a++;
+//	}
+//	cout << a << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <assert.h>
+//using namespace std;
+//
+////找数组中是否有该数，数组行增长，列增长
+//void Fnid_val(int *arr, int row, int col, int num)
+//{
+//	assert(arr); //先判断数组不为空
+//	if (row > 0 && col > 0)
+//	{
+//		int tmpRow = 0; //行
+//		int tmpCol = col - 1; //列 3
+//		while (tmpRow < row && col >= 0)
+//		{
+//			if (arr[tmpRow * col + tmpCol] == num) //刚好为该数
+//			{
+//				printf("%d,intdex = %d\n", arr[tmpRow * col + tmpCol], tmpRow * col + tmpCol);
+//				break;
+//			} 
+//			else if (arr[tmpRow * col + tmpCol] > num) //列减一往前找
+//			{
+//				--tmpCol;
+//			} 
+//			else //否则往下一列找
+//			{
+//				++tmpRow;
+//			}
+//		}
+//	}
+//} 
+//
+//int main()
+//{
+//	int arr[4][4] = { 1, 2, 8, 9,	
+//					  2, 4, 9, 12,
+//					  4, 7, 10, 13,
+//					  6, 8, 11, 15 };
+//	Fnid_val((int*)arr, 4, 4, 15);
+//	system("pause");
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <assert.h>
+//using namespace std;
+//
+////找数组中是否有该数，数组行增长，列增长
+//void Fnid_val(int *arr, int row, int col, int num)
+//{
+//	assert(arr); 
+//	if (row > 0 && col > 0)
+//	{
+//		int tmprow = 0;
+//		int tmpcol = col - 1;
+//		while (tmprow < row && tmpcol >= 0)
+//		{
+//			if (arr[tmprow * col + tmpcol] == num)
+//			{
+//				cout << "找到了" << endl;
+//				break;
+//			}	
+//			else if (arr[tmprow * col + tmpcol] > num)
+//				--tmpcol;
+//			else
+//				++tmprow;
+//		}
+//	}
+//} 
+//
+//int main()
+//{
+//	int arr[4][4] = { 1, 2, 8, 9,	
+//					  2, 4, 9, 12,
+//					  4, 7, 10, 13,
+//					  6, 8, 11, 15 };
+//	Fnid_val((int*)arr, 4, 4, 11);
+//	system("pause");
+//	return 0;
+//}
+
+//找第二高
+//#include <iostream>
+//#include <vector>
+//#include  <algorithm>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	while (cin >> n)
+//	{
+//		vector<int> arr(3 * n);
+//		for (int i = 0; i < 3 * n; i++)
+//			cin >> arr[i];
+//
+//		int sum = 0;
+//		sort(arr.begin(), arr.end());
+//		for (int i = n; i <= 3 * n - 2; i = i + 2) 
+//		{
+//			sum = sum + arr[i];
+//		}
+//		cout << sum << endl;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	string s1, s2;
-	while (cin >> s1 >> s2)
+	string s1;
+	string s2;
+	getline(cin, s1);
+	getline(cin, s2);
+	char arr[256] = { 0 }; //开始下标为0
+	for (int i = 0; i < s2.size(); i++)
 	{
-		bool status = true;
-		map<char, int> str1, str2;
-		for (int i = 0; i < s1.size(); i++)
-			str1[s1[i]]++;
-
-		for (int j = 0; j < s2.size(); j++)
-			str2[s2[j]]++;
-
-		map<char, int>::iterator it = str2.begin();
-		while (it != str2.end())
-		{
-			if (str1[it->first] < it->second) //str1 < str2长度则退出，否则继续往下走
-			{
-				status = false;
-				break;
-			}
-			++it;
-		}
-
-		if (status== true )
-			cout << "Yes" << endl;
-		else
-			cout << "No" << endl;
+		arr[s2[i]]++;
 	}
+	string tmp;
+	for (int i = 0; i < s1.size(); i++)
+	{
+		if (arr[s1[i]] == 0)
+			tmp += s1[i];
+	}
+	cout << tmp << endl;
 	system("pause");
 	return 0;
 }
