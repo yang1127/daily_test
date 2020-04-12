4aa74d02d5950d0a7f1de3521d84d7a9138e4b52
yang1127 <1536520643@qq.com>
2020-04-12 23:16:28 +0800

    练习

--- day05/day05/test05.cpp
+++ day05/day05/test05.cpp
@@ -43,4 +43,81 @@
 //	return 0;
 //}
 
-
+//typedef struct {
+//	int* _array; //数组
+//	int _n;     //大小
+//	int _front; //队头
+//	int _rear;  //队尾  
+//} MyCircularQueue;
+//
+//
+///** Initialize your data structure here. Set the size of the queue to be k. */
+//MyCircularQueue* myCircularQueueCreate(int k) {
+//	MyCircularQueue* queue = (MyCircularQueue*)malloc(sizeof(MyCircularQueue));
+//	queue->_array = (int*)malloc(sizeof(int)*(k + 1));
+//	queue->_n = (k + 1);
+//	queue->_front = 0;
+//	queue->_rear = 0;
+//
+//	return queue;
+//}
+//
+//
+///** Checks whether the circular queue is empty or not. */
+//bool myCircularQueueIsEmpty(MyCircularQueue* obj) {
+//	return obj->_rear == obj->_front; //相等为空
+//}
+//
+//
+///** Checks whether the circular queue is full or not. */
+//bool myCircularQueueIsFull(MyCircularQueue* obj) {
+//	return (obj->_rear + 1) % obj->_n == obj->_front ? true : false; //+1为满
+//}
+//
+//
+///** Insert an element into the circular queue. Return true if the operation is successful. */
+//bool myCircularQueueEnQueue(MyCircularQueue* obj, int value) {
+//	if (myCircularQueueIsFull(obj) == true)
+//		return false;
+//
+//	obj->_array[obj->_rear++] = value;
+//	obj->_rear %= obj->_n;
+//	return true;
+//}
+//
+//
+///** Delete an element from the circular queue. Return true if the operation is successful. */
+//bool myCircularQueueDeQueue(MyCircularQueue* obj) {
+//	if (myCircularQueueIsEmpty(obj) == true)
+//		return false;
+//
+//	++obj->_front;
+//	obj->_front %= obj->_n;
+//	return true;
+//}
+//
+//
+///** Get the front item from the queue. */
+//int myCircularQueueFront(MyCircularQueue* obj) {
+//	if (myCircularQueueIsEmpty(obj) == true)
+//		return -1;
+//	else
+//		return obj->_array[obj->_front];
+//}
+//
+//
+///** Get the last item from the queue. */
+//int myCircularQueueRear(MyCircularQueue* obj) {
+//	if (myCircularQueueIsEmpty(obj) == true)
+//		return -1;
+//	else
+//		if (obj->_rear == 0)
+//			return obj->_array[obj->_n - 1];
+//		else
+//			return obj->_array[obj->_rear - 1];
+//}
+//
+//void myCircularQueueFree(MyCircularQueue* obj) {
+//	free(obj->_array);
+//	free(obj);
+//}
