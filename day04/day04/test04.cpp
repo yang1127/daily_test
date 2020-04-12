34a24627c1df36126c4bf209cdce1bbb16076754
yang1127 <1536520643@qq.com>
2020-04-12 14:32:44 +0800

    练习

--- day04/day04/test04.cpp
+++ day04/day04/test04.cpp
@@ -592,3 +592,191 @@
 //	StackDestroy(&obj->popST);
 //	free(obj);
 //}
+
+typedef int QUDataType;
+
+typedef struct QueueNode {
+	struct QueueNode* _next;
+	QUDataType _data;
+}QueueNode;
+
+typedef struct Queue {
+	QueueNode* _front; // 队头
+	QueueNode* _tail; // 队尾
+}Queue;
+
+
+void QueueInit(Queue* pq) //初始化
+{
+	assert(pq);
+	pq->_front = NULL;
+	pq->_tail = NULL;
+}
+
+
+void QueueDestroy(Queue* pq) //销毁
+{
+	QueueNode* cur = pq->_front;
+	while (cur != NULL)  //所有链表节点释放掉
+	{
+		QueueNode* next = cur->_next;
+		free(cur);
+		cur = next; //迭代
+	}
+	pq->_front = NULL;
+	pq->_tail = NULL;
+}
+
+
+QueueNode* BuyQueueNode(QUDataType x) //置新节点
+{
+	QueueNode* node = ((QueueNode*)malloc(sizeof(QueueNode)));//新节点开辟空间
+	node->_data = x; //新节点元素赋值
+	node->_next = NULL;
+	return node;
+}
+
+
+void QueuePush(Queue* pq, QUDataType x) //入队:队尾入数据
+{
+	QueueNode* newnode = BuyQueueNode(x);
+	if (pq->_tail == NULL) //开始定义tail为空
+	{
+		pq->_front = pq->_tail = newnode; //队里无数据时，队头队尾的值均为新节点
+	}
+	else
+	{
+		pq->_tail->_next = newnode;
+		pq->_tail = newnode;  //队里有数据，直接队尾插入新节点
+	}
+}
+
+
+void QueuePop(Queue* pq) //出队：队头出数据，删掉队头数据，先保存下一个数据
+{
+	assert(pq && pq->_front != NULL);
+
+	QueueNode* next = pq->_front->_next;
+	free(pq->_front);
+	pq->_front = next;
+
+	if (pq->_front == NULL) //如果队头为空，将尾置为空，否则为野指针
+	{
+		pq->_tail = NULL; //队头为空，将队尾的指针指向NULL，否则为野指针
+	}
+}
+
+
+QUDataType QueueFront(Queue* pq) //取队头的数据
+{
+	assert(pq && pq->_front != NULL);
+	return pq->_front->_data;
+}
+
+
+QUDataType QueueBack(Queue* pq) //取队尾的数据
+{
+	assert(pq && pq->_tail != NULL);
+	return pq->_tail->_data;
+}
+
+
+int QueueEmpty(Queue* pq) //判断队是否为空
+{
+	assert(pq);
+	return pq->_front == NULL ? 0 : 1; //没有对头节点即为空 O(1)
+	// return QueueSize(pq) == 0; //O(n)
+}
+
+
+int QueueSize(Queue* pq) //队列大小
+{
+	assert(pq);
+	QueueNode* cur = pq->_front;
+	size_t size = 0;
+	while (cur != NULL)
+	{
+		++size;
+		cur = cur->_next;
+	}
+
+	return size;
+}
+
+typedef struct {
+	Queue _q1; //两个队列
+	Queue _q2;
+} MyStack;
+
+/** Initialize your data structure here. */
+
+MyStack* myStackCreate() {
+	MyStack* st = (MyStack*)malloc(sizeof(MyStack));
+	QueueInit(&st->_q2);
+	QueueInit(&st->_q1); //初始化
+	return st;
+}
+
+/** Push element x onto stack. */
+void myStackPush(MyStack* obj, int x) {
+	if (QueueEmpty(&obj->_q1) != 0) //哪个队列不为空，入栈
+	{
+		QueuePush(&obj->_q1, x);
+	}
+	else
+	{
+		QueuePush(&obj->_q2, x);
+	}
+}
+
+/** Removes the element on top of the stack and returns that element. */
+int myStackPop(MyStack* obj) {
+	Queue* empty = &obj->_q1; //默认q1为空、q2不为空
+	Queue* noempty = &obj->_q2;
+	if (QueueEmpty(&obj->_q1) != 0)
+	{
+		empty = &obj->_q2;
+		noempty = &obj->_q1;
+	}
+
+	while (QueueSize(noempty) > 1)
+	{
+		QueuePush(empty, QueueFront(noempty)); //将非空往空里面导
+		QueuePop(noempty);
+	}
+	int front = QueueFront(noempty);
+	QueuePop(noempty);
+	return front;
+}
+
+/** Get the top element. */
+int myStackTop(MyStack* obj) {
+	Queue* empty = &obj->_q1; //默认q1为空、q2不为空
+	Queue* noempty = &obj->_q2;
+	if (QueueEmpty(&obj->_q1) != 0)
+	{
+		empty = &obj->_q2;
+		noempty = &obj->_q1;
+	}
+
+	while (QueueSize(noempty) > 1)
+	{
+		QueuePush(empty, QueueFront(noempty));
+		QueuePop(noempty);
+	}
+	int front = QueueFront(noempty);
+	QueuePush(empty, front); //队列最后一个数据，为栈顶元素
+	QueuePop(noempty);
+	return front;
+}
+
+/** Returns whether the stack is empty. */
+bool myStackEmpty(MyStack* obj) {
+	return QueueEmpty(&obj->_q1) + QueueEmpty(&obj->_q2) == 0 ? true : false;
+}
+
+void myStackFree(MyStack* obj) {
+	QueueDestroy(&obj->_q1);
+	QueueDestroy(&obj->_q2);
+	free(obj);
+}
