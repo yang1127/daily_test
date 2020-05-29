1d8030adf1d12e4df6fbb4887d5f27e3ae087e35
yang1127 <1536520643@qq.com>
2020-05-29 22:50:20 +0800

    日常练习

--- day08/day08/test.cpp
+++ day08/day08/test.cpp
@@ -460,3 +460,21 @@
 //		return canBreak[len];
 //	}
 //};
+
+#include<stdio.h>
+
+int main()
+{
+	long long der[21] = { 0, 0, 1 };
+	int i;
+	for (i = 3; i < 21; i++) 
+	{
+		der[i] = (i - 1) * (der[i - 2] + der[i - 1]);
+	}
+	int n;
+	while (scanf("%d", &n) != EOF) 
+	{
+		printf("%lld\n", der[n]);
+	}
+	return 0;
+}
