c36cc0f95d9c3cbcbb941ff6ed326c6f9ca47599
yang1127 <1536520643@qq.com>
2020-06-08 22:57:44 +0800

    日常练习

--- day09/day09/test.cpp
+++ day09/day09/test.cpp
@@ -13,25 +13,53 @@
 //}
 //};
 
-#include<iostream> 
+//#include<iostream> 
+//using namespace std;
+//
+//int pathNum(int n,int m) 
+//{
+//	if(n > 1 && m > 1) 
+//		return pathNum(n-1,m) + pathNum(n,m-1); 
+//	else if(((n >= 1)&&(m == 1))||((n == 1)&&(m >= 1))) 
+//		return n + m; 
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int n,m; 
+//	while(cin>>n>>m)
+//	{
+//		cout<<pathNum(n,m)<<endl;
+//	}
+//	
+//	return 0; 
+//}
+
+//查找第K小数
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int pathNum(int n,int m) 
-{
-	if(n > 1 && m > 1) 
-		return pathNum(n-1,m) + pathNum(n,m-1); 
-	else if(((n >= 1)&&(m == 1))||((n == 1)&&(m >= 1))) 
-		return n + m; 
-	else
-		return 0;
-}
 int main()
 {
-	int n,m; 
-	while(cin>>n>>m)
+	int n, nums, k;
+	while (cin >> n)
 	{
-		cout<<pathNum(n,m)<<endl;
+		vector <int> v;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> nums;
+			v.push_back(nums);
+		}
+			
+		cin >> k;
+		sort(v.begin(), v.end());
+		unique(v.begin(), v.end());
+		cout << v[k - 1];
 	}
-	
-	return 0; 
+
+	system("pause");
+	return 0;
 }
