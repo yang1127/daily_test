44091cd2e8e81e8f2781e32bd123af377ff9d82e
yang1127 <1536520643@qq.com>
2020-08-23 23:22:17 +0800

    日常练习

--- day15/day15/test.cpp
+++ day15/day15/test.cpp
@@ -1,3 +1,4 @@
+/*
 class Gift {
 public:
 	int getValue(vector<int> gifts, int n) {
@@ -38,3 +39,166 @@ public:
 			return 0;
 	}
 };
+*/
+
+/*
+//链表逆序返回数组
+class Solution {
+public:
+	vector<int> reversePrint(ListNode* head) {
+		stack<int> st;
+		vector<int> v;
+		while (head)
+		{
+			st.push(head->val);
+			head = head->next;
+		}
+
+		while (!st.empty())
+		{
+			v.push_back(st.top());
+			st.pop();
+		}
+
+		return v;
+	}
+};
+
+//逆序数组
+class Solution {
+public:
+	vector<int> reversePrint(ListNode* head) {
+		vector<int> v;
+		while (head)
+		{
+			v.push_back(head->val);
+			head = head->next;
+		}
+
+		reverse(v.begin(), v.end());
+		return v;
+
+	}
+};
+*/
+
+/*
+//反转链表
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		//头插
+		ListNode* cur = head;
+		ListNode* newnode = NULL;
+
+
+		//NUll
+		//1->NULL
+		//2->1->NULL
+		//3->2->1->NULL
+		while (cur)
+		{
+			ListNode* next = cur->next;
+			cur->next = newnode;
+
+			newnode = cur;
+			cur = next;
+		}
+		return newnode;
+	}
+};
+*/
+
+/*
+//二叉搜索树的第k大节点
+class Solution {
+public:
+	vector<int> v;
+	void Middle(TreeNode* root)
+	{
+		if (root == NULL)
+			return;
+
+		Middle(root->left);
+		v.push_back(root->val);
+		Middle(root->right);
+	}
+
+	int kthLargest(TreeNode* root, int k) {
+		//求 “二叉搜索树第 k 大的节点” 可转化为求 “此树的中序遍历倒序的第 k 个节点
+		Middle(root);
+
+		return v[v.size() - k];
+	}
+};
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
