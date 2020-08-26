c60707101847b685496e2c9b1104f8f7dd6f6253
yang1127 <1536520643@qq.com>
2020-08-26 21:30:16 +0800

    日常练习

--- day15/day15/test.cpp
+++ day15/day15/test.cpp
@@ -133,7 +133,52 @@ public:
 };
 */
 
+//计算二进制1的个数
+//class Solution {
+//public:
+//	int hammingWeight(uint32_t n) {
+//		int count = 0;
+//		while (n)
+//		{
+//			n = n & (n - 1);
+//			count++;
+//		}
+//		return count;
+//	}
+//};
 
+//class Solution {
+//public:
+//	int majorityElement(vector<int>& nums) {
+//		sort(nums.begin(), nums.end());
+//		int num = nums[nums.size() / 2];
+//		return num;
+//	}
+//};
+
+//class Solution {
+//public:
+//	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+//		if (l1 == NULL)
+//			return l2;
+//
+//		if (l2 == NULL)
+//			return l1;
+//
+//		if (l1->val <= l2->val)
+//		{
+//			l1->next = mergeTwoLists(l1->next, l2);
+//			return  l1;
+//		}
+//		else
+//		{
+//			l2->next = mergeTwoLists(l1, l2->next);
+//			return l2;
+//		}
+//
+//
+//	}
+//};
 
 
 
