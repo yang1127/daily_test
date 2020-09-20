d41384587c2747d20e466f7e9619fdfc3215b81c
yang1127 <1536520643@qq.com>
2020-09-20 23:45:14 +0800

    日常练习

--- day17/day17/test.cpp
+++ day17/day17/test.cpp
@@ -74,40 +74,123 @@
 //	return 0;
 //}
 
+//#include <iostream>
+//#include <string>
+//#include <ctype.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string s;
+//	getline(cin, s);
+//	int count = 0;
+//	string tmp = "";
+//	tmp = s[0];
+//	for (int i = 0; i < s.size(); i++)
+//	{
+//		s[i] = tolower(s[i]); //תСд
+//		if (s[i] != ' ' && (s[i] < 'a' || s[i] > 'z'))
+//		{
+//			cout << "unkown name" << endl;
+//			break;
+//		}
+//
+//		if (s[i] == ' ')
+//		{
+//			count++;
+//			s[i + 1] = tolower(s[i + 1]);
+//			tmp += s[i + 1];
+//		}
+//	}
+//
+//	if (count < 2)
+//		cout << s << endl;
+//	else
+//		cout << tmp << endl;
+//
+//	system("pause");
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string s1;
+//	string s2;
+//	cin >> s1;
+//	cin >> s2;
+//	int count = 0;
+//	char arr[256] = { 0 };
+//	for (int i = 0; i < s1.size(); i++)
+//		arr[s1[i]]++;
+//
+//	string tmp = "";
+//	for (int j = 0; j < s2.size(); j++)
+//	{
+//		if (arr[s2[j]] == 0)
+//			tmp += s2[j];
+//	}
+//	count = s2.size() - tmp.size();
+//
+//	cout << count << endl;
+//
+//	system("pause");
+//	return 0;
+//}
+
 #include <iostream>
 #include <string>
-#include <ctype.h>
 
 using namespace std;
 
 int main()
 {
-	string s;
-	getline(cin, s);
-	int count = 0;
-	string tmp = "";
-	tmp = s[0];
-	for (int i = 0; i < s.size(); i++)
+	int n;
+	cin >> n;
+	while (n)
 	{
-		s[i] = tolower(s[i]); //תСд
-		if (s[i] != ' ' && (s[i] < 'a' || s[i] > 'z'))
+		string s;
+		cin >> s;
+		int flag = 0;
+		char arr[256] = { 0 };
+
+		string tmp = s.substr(0, 3);
+		for (int i = 0; i < tmp.size(); i++)
 		{
-			cout << "unkown name" << endl;
-			break;
+			arr[tmp[i]]++;
 		}
 
-		if (s[i] == ' ')
+		for (int j = 3; j < s.size(); j += 3)
 		{
-			count++;
-			s[i + 1] = tolower(s[i + 1]);
-			tmp += s[i + 1];
+			string tmp1 = "";
+			tmp1 = s.substr(j, 3);
+
+			string tmp2 = "";
+			for (int k = 0; k < tmp1.size(); j++)
+			{
+				if (arr[tmp[k]] == 0)
+					tmp2 += tmp1[k];
+			}
+			int count = tmp1.size() - tmp2.size();
+			if (count == 2)
+				flag = 1;
+			else
+				flag = 0;
+			
 		}
+
+		if (flag == 1)
+			cout << "Yes" << endl;
+		else
+			cout << "No" << endl;
+
+		n--;
 	}
-
-	if (count < 2)
-		cout << s << endl;
-	else
-		cout << tmp << endl;
 
 	system("pause");
 	return 0;
